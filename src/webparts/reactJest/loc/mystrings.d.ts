8c6ebd853e9b5b9a4dfbc10b75b46ee25bb133a9
Velin Georgiev <velin.georgiev@gmail.com>
2018-04-03 23:17:14 +0100

    Jest - first test pass when npm test

---
+++ src/webparts/reactJest/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IReactJestWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'ReactJestWebPartStrings' {
+  const strings: IReactJestWebPartStrings;
+  export = strings;
+}
---
+++ src/webparts/reactJest/test/ReactJest.test.ts
@@ -0,0 +1,8 @@
+/// <reference types="jest" />
+
+
+describe("GET /api", () => {
+    it("simple test", () => {
+        expect(1+2).toBe(3);
+    });
+  });
