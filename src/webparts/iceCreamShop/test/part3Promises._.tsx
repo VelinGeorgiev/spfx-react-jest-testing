19e7e6e949dd758911e44fb7697f201aadb7971d
Velin Georgiev <velin.georgiev@gmail.com>
2018-05-09 21:27:16 +0100

    unit test fixes

---
+++ src/webparts/iceCreamShop/test/part3Promises._.tsx
@@ -0,0 +1,55 @@
+// /// <reference types="jest" />
+// /// <reference types="enzyme" />
+
+// import * as React from 'react';
+// import * as Enzyme from 'enzyme';
+// import * as Adapter from 'enzyme-adapter-react-15';
+// import { IIceCreamShopProps } from '../components/IIceCreamShopProps';
+// import { IIceCreamShopState } from '../components/IIceCreamShopState';
+// import IceCreamShop from '../components/IceCreamShop';
+// import { IceCreamFakeProvider } from '../iceCreamProviders/IceCreamFakeProvider';
+
+// describe('Promises basics', () => {
+
+//   let reactComponent: Enzyme.ReactWrapper<IIceCreamShopProps, IIceCreamShopState>;
+
+//   beforeEach(() => {
+
+//     reactComponent = Enzyme.mount(React.createElement(
+//       IceCreamShop,
+//       {
+//         title: "Test title",
+//         iceCreamProvider: new IceCreamFakeProvider()
+//       }
+//     ));
+//   });
+
+//   it('should show a list of ice cream flavours', (done) => {
+
+//     setTimeout(_ => {
+
+//       const componentAsDOM = reactComponent.getDOMNode();
+//       const flavours = componentAsDOM.querySelectorAll("[class*='flavourLabel']");
+
+//       expect(flavours[0].textContent).toBe(" Chocolate");
+//       expect(flavours[1].textContent).toBe(" Coffee and Cookie");
+//       expect(flavours[2].textContent).toBe(" Vanilla");
+
+//       done();
+
+//     }, 10);
+//   });
+
+//   it('should state of iceCreamFlavoursList changed', (done) => {
+    
+//       setTimeout(_ => {
+  
+//         const flavoursState = reactComponent.state().iceCreamFlavoursList;
+  
+//         expect(flavoursState.length).toBe(4);
+  
+//         done();
+  
+//       }, 10);
+//     });
+// });
---
+++ src/webparts/iceCreamShop/test/part4SinonSpiesBasic._.tsx
@@ -0,0 +1,38 @@
+// /// <reference types="jest" />
+// /// <reference types="sinon" />
+
+// import * as React from 'react';
+// import { assert } from 'chai';
+// import { mount, ReactWrapper } from "enzyme";
+// import IceCreamMultiplier from '../IceCreamMultiplier';
+
+// declare const sinon;
+
+// describe('Sinon basic spy', () => {
+
+//   let multiplier: IceCreamMultiplier;
+//   let multiplyByTwoSpy: sinon.SinonSpy;
+
+//   beforeEach(() => {
+
+//     multiplyByTwoSpy = sinon.spy(IceCreamMultiplier.prototype, "multiplyByTwo");
+
+//     multiplier = new IceCreamMultiplier();
+//     multiplier.multiply(2);
+//   });
+
+//   afterEach(() => {
+//     multiplyByTwoSpy.restore();
+//   });
+
+//   it('should multiplier input', () => {
+
+//     assert(multiplyByTwoSpy.calledWith(2));
+//   });
+
+//   it('should multiplier output', () => {
+
+//     assert(multiplyByTwoSpy.returned(4));
+//   });
+// });
+// // http://sinonjs.org/
