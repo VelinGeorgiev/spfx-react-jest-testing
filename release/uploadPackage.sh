2ccc4990e9d9c833a51a0c2d11b22e774ae5dca4
Velin Georgiev <velin.georgiev@gmail.com>
2018-10-16 00:18:27 +0100

    script updated to upload app the the app catalog

--- release/uploadPackage.sh
+++ release/uploadPackage.sh
@@ -1,2 +1,14 @@
-npm i -g @pnp/office365-cli 
+EMAIL=$1
+PASS=$2
+
+npm i -g @pnp/office365-cli
 o365 version
+
+# you have to run "o365 spo login https://contoso.sharepoint.com"
+# to agree with the consent first time
+
+o365 spo login https://velingeorgiev.sharepoint.com --authType password --userName $EMAIL --password $PASS
+
+o365 spo app add --filePath "../SPFx sppkg/react-jest-testing.sppkg" --overwrite --verbose
+
+#o365 spo app remove -i 35625ec7-e7bf-4f54-975a-cfa6c057015f --verbose
