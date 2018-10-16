23c0aebfdecfb7d61398ab3db2c6999ff25a0308
Velin Georgiev <velin.georgiev@gmail.com>
2018-10-16 22:40:34 +0100

    scripts refactored . readme added

--- devops/release/office-365-CLI-add-app.sh
+++ devops/release/office-365-CLI-add-app.sh
@@ -1,5 +1,6 @@
-EMAIL=$1
-PASS=$2
+SITE_URL=$1
+EMAIL=$2
+PASS=$3
 
 npm i -g @pnp/office365-cli
 o365 version
@@ -7,6 +8,6 @@ o365 version
 # you have to run "o365 spo login https://contoso.sharepoint.com"
 # to agree with the consent first time
 
-o365 spo login https://velingeorgiev.sharepoint.com --authType password --userName $EMAIL --password $PASS
+o365 spo login $SITE_URL --authType password --userName $EMAIL --password $PASS
 
 o365 spo app add --filePath "./_SPFx build/SPFx sppkg/react-jest-testing.sppkg" --overwrite --verbose
