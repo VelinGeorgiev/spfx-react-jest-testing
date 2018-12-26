5da3185525f777f0bbc50d6415a81d6fb7705a8d
Velin Georgiev <velin.georgiev@gmail.com>
2018-12-26 20:10:14 +0000

    title changed to 4

--- src/webparts/iceCreamShop/loc/en-us.js
+++ src/webparts/iceCreamShop/loc/en-us.js
@@ -8,6 +8,6 @@ define([], function() {
     "ForLabel": "for",
     "BuyLabel": "Buy",
     "SuccessLabel": "Success",
-    "TitleLabel": "PnP Ice Cream Shop 3"
+    "TitleLabel": "PnP Ice Cream Shop 4"
   }
 });
