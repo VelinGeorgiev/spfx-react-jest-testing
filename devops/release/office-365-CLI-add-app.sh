32f0c7c9bc7511d260b97ef4b780c3a0e307d050
Velin Georgiev <velin.georgiev@gmail.com>
2019-02-19 00:08:06 +0000

    scope added as param to the release scripts

--- devops/release/office-365-CLI-add-app.sh
+++ devops/release/office-365-CLI-add-app.sh
@@ -1,6 +1,7 @@
 SITE_URL=$1
 EMAIL=$2
 PASS=$3
+SCOPE=$4
 
 npm i -g @pnp/office365-cli
 o365 version
@@ -10,6 +11,6 @@ o365 version
 
 o365 spo login $SITE_URL --authType password --userName $EMAIL --password $PASS
 
-o365 spo app add --filePath "./_SPFx build/SPFx sppkg/react-jest-testing.sppkg" --overwrite
+o365 spo app add --filePath "./_SPFx build/SPFx sppkg/react-jest-testing.sppkg" --scope $SCOPE --overwrite
 
-o365 spo app deploy --name react-jest-testing.sppkg --skipFeatureDeployment
+o365 spo app deploy --name react-jest-testing.sppkg --scope $SCOPE --skipFeatureDeployment
